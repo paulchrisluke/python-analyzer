02263ef8125eac8b614102aaf0e406e459b49aea
Paul Chris Luke <paulchrisluke@gmail.com>
2025-09-19 12:41:14 +0700

    Add Help Scout beacon integration
    - Added Help Scout beacon script to root layout
    - Properly integrated according to Help Scout documentation
    - App-wide coverage for customer support chat widget

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -38,6 +38,16 @@ export default function RootLayout({
         <NextAuthProvider>
           {children}
         </NextAuthProvider>
+        {/* Help Scout Beacon Script */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],t.readyState==="complete")return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon);
+              window.Beacon('init', '72f141e2-2630-4d7d-b166-246e8d318a2e');
+            `,
+          }}
+        />
       </body>
     </html>
   );
